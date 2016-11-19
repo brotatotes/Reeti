39a6c2932fa4a7822df7cfd69e239f99fbc22933
Dylan Ong <Dylan Ong>
2016-11-19 17:06:15 -0600

    added some ingredients

--- Ingredients.js
+++ Ingredients.js
@@ -1,4 +1,10 @@
-var Ingredients_List = [];
+var Ingredients_List = ["canola oil", "ketchup", "olive oil", "salt", "chili",
+ "soy sauce", "hot sauce", "caper", "vanilla", "olive", "peanut butter", "jelly",
+ "tomato", "salsa", "tuna", "rice", "sugar", "butter", "cheese", "egg", "milk",
+ "yogurt", "tortilla", "blueberry", "parmesan", "mozzarella", "flour", "spaghetti",
+ "meatball", "pasta", "fettucine", "chicken", "pork", "ground beef", "beef", "pea",
+ "strawberries", "almond", "walnut", "lemon", "orange", "apple", "banana", "potato", "mushroom"
+ "avocado"];
 
 module.exports = {
 	Ingredients_List: Ingredients_List
