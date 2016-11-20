0ef1cfc2295b38fe5a831c79529792922c3c810d
Adam He <shuaihe2018@u.northwestern.edu>
2016-11-20 13:46:26 -0600

    new key

--- scripts/ClarifaiStore.js
+++ scripts/ClarifaiStore.js
@@ -10,7 +10,7 @@ var RecipeStore = require('./RecipeStore.js');
 function getIngredients(event, context, callback){
 	var url = "https://api.clarifai.com/v1/tag"
 	var image_test = event['body-json'].image;
-	var access_token = "iK0QKs33uO9shdmEW4UH9iMV8BPSaF"
+	var access_token = "IwVpi5Xi5fSnR627s6qniLauqh9nIV"
 	var model = "food-items-v1.0"
 	request.get({
 		url: url + "?" + "model=" + model + "&" + "url=" + image_test + "&" + "access_token=" + access_token
