d633deb5dbac9451b9f7b48ab825db32bb6b251f
brotatotes <hao.awesome@gmail.com>
2016-11-20 00:44:45 -0600

    added to Ingredients.js

--- scripts/Ingredients.js
+++ scripts/Ingredients.js
@@ -6,8 +6,12 @@ var Ingredients_List = ["green bean", "onion", "pepper", "vegetable oil", "canol
   "meatball", "pasta", "fettucine", "chicken", "pork", "ground beef", "beef", "pea",
   "strawberries", "almond", "walnut", "lemon", "orange", "apple", "banana", "potato",
   "mushroom", "avocado", "garlic", "ginger", "green onion", "basil", "oregano", "thyme",
-  "cucumber", "corn", "fish", "asparagus", "mayonnaise", "mustard", "wasabi", "salmon", "sashimi", "seaweed",
-  "shellfish", "cashew", "clam", "soy", "tofu"];
+  "cucumber", "corn", "fish", "asparagus", "mayonnaise", "mustard", "wasabi", "salmon", 
+  "sashimi", "seaweed", "shellfish", "cashew", "clam", "soy", "tofu", "peanut", "lamb",
+  "bean", "lentil", "tapioca", "carrot", "eggplant", "peach", "pear", "kiwi", "sausage"
+  "shrimp", "zucchini", "kale", "cinnamon", "raspberry", "pineapple", "date", "cilantro"
+  ];
+
 module.exports = {
   Ingredients_List: Ingredients_List
 }
