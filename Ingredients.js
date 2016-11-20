a63753f3229e79338f3ad81d2a481e14e291b095
softkaiser <austinchambers1@gmail.com>
2016-11-19 18:31:36 -0600

    added like all functionality

--- Ingredients.js
+++ Ingredients.js
@@ -3,7 +3,7 @@ var Ingredients_List = ["canola oil", "ketchup", "olive oil", "salt", "chili",
  "tomato", "salsa", "tuna", "rice", "sugar", "butter", "cheese", "egg", "milk",
  "yogurt", "tortilla", "blueberry", "parmesan", "mozzarella", "flour", "spaghetti",
  "meatball", "pasta", "fettucine", "chicken", "pork", "ground beef", "beef", "pea",
- "strawberries", "almond", "walnut", "lemon", "orange", "apple", "banana", "potato", "mushroom"
+ "strawberries", "almond", "walnut", "lemon", "orange", "apple", "banana", "potato", "mushroom",
  "avocado"];
 
 module.exports = {
--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var ClarifaiService = require("./ClarifaiService.js");
 var RecipeService = require("./RecipeService.js");
+var ClarifaiStore = require("./ClarifaiStore.js");
 
 function route(event, context, callback){
 	switch(event.operation){
 		case "get_ingredients":
-		ClarifaiService.getIngredients(event, context, callback);
+		ClarifaiStore.getIngredients(event, context, callback);
 		break;
-
 		case "get_recipes":
 		RecipeService.getRecipes(event, context, callback);
 		break;
-
 		default:
 		callback(new Error("Unkown operation"));
 		console.log("Unknown operation");
